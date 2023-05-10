2918463c654bc932c820f7acf8565c9b94c62dc9
ya0igoddess <matratskingalya@gmail.com>
2023-05-10 23:57:47 +0300

    app gradle improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,6 @@ group = rootProject.group
 version = rootProject.version
 java.sourceCompatibility = JavaVersion.VERSION_17
 
-val kotlinVersion: String by project
-val kordVersion: String by project
-
 plugins {
     id("org.springframework.boot")
     id("io.spring.dependency-management")
@@ -26,9 +23,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 
     //skaard dependencies
     implementation(project(":template-module"))
